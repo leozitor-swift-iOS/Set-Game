1a4adaa2ae2e1cef7351d0bf151ee84305669fb2
Leozitor Floro de Souza <leozitor@gmail.com>
2020-09-25 13:06:12 -0400

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Leozítor Floro on 25/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Leozítor Floro on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
